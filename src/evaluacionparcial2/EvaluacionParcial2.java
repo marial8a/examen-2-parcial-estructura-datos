f34b0d9167afe5c2675656b25315ae4c57227693
marial8a <112141999+marial8a@users.noreply.github.com>
2023-03-16 01:28:58 -0500

    Linea base examen
    Falta realizar
    
    3.Multiplicacion de una matriz por un número (el tamaño de la matriz lo define el usuario), tanto la matriz como el numero multiplicador debe ser ingresado por teclado.
    
    4.LLenar una matriz  de nxm, porteriormente imprimir los numeros ingresados que sean pares.

---
+++ src/evaluacionparcial2/EvaluacionParcial2.java
@@ -0,0 +1,11 @@
+
+package evaluacionparcial2;
+
+public class EvaluacionParcial2 {
+
+    public static void main(String[] args) {
+        Solucion solucion = new Solucion();
+        solucion.setVisible(true);
+    }
+    
+}
